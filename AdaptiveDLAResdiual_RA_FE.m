e44231a2abd952d128858d541697b4137eba863d
Stefan Schnake <stefan.schnake@gmail.com>
2022-04-22 13:44:43 -0400

    Starting to modify Adaptivity routines.  Not complete yet

--- AdaptiveDLAResdiual_RA_FE.m
+++ AdaptiveDLAResdiual_RA_FE.m
@@ -1,4 +1,4 @@
-function [C,S,D] = AdaptiveDLAResdiual_RA_FE(x,v,k,C,S,D,dt,tol,Awave,BC)
+function [U] = AdaptiveDLAResdiual_RA_FE(U,dt,tol,Awave,BC)
 %Adaptive algorithm for DLA update
 
 %Parameters
@@ -10,27 +10,30 @@ quiet = false;
 
 r = size(S,1);
 if quiet; fprintf('-- Adaptive: r = %d\n',r); end
-C0 = C; S0 = S; D0 = D;
+U0 = SOLN(U.x,U.v,U.k);
+R  = SOLN(U.x,U.v,U.k);
+R_tmp = SOLN(U.x,U.v,U.k);
+U0.C = U.C; U0.S = U.S; U0.D = U.D;
 %Update
 tic
-[C,S,D] = DLA4_HB_FE(x,v,k,C,S,D,dt,Awave,BC);
+U = DLA4_HB_FE(U,dt,Awave,BC);
 %[C,S,D] = DLA4_HB_SSP_RK2(x,v,k,C,S,D,dt,Awave,BC);
 %[C,S,D] = DLA5_HB_FE(x,v,k,C,S,D,dt,Awave,BC);
 %Compute SVD of S
 %[S_C,Sig,S_D] = svd(S);
 time1 = toc;
 %fprintf(' DLA update time: %f\n',toc);
-Rnt = @(x) firstOrderResidual(C0,S0,D0,Awave,dt,BC,x,'notransp',C,D);
-Rt = @(x) firstOrderResidual(C0,S0,D0,Awave,dt,BC,x,'transp',C,D);
+Rnt = @(x) firstOrderResidual(U0,Awave,dt,BC,x,'notransp',U);
+Rt = @(x) firstOrderResidual(U0,Awave,dt,BC,x,'transp',U);
 tic
-Q = randn(size(C,1),pp);
+Q = randn(size(U.C,1),pp);
 Q = Rnt(Q);
 [Q,~] = qr(Q,0);
 B = Rt(Q)';
-[Utmp,S1,D1] = svd(B,'econ');
-C1 = Q*Utmp;
+[Utmp,R.S,R.D] = svd(B,'econ');
+R.C = Q*Utmp;
 time1 = toc;
-normF = norm(diag(S1),2) + sqrt(max([6*r-p,1]))*S1(end,end);
+normF = norm(diag(R.S),2) + sqrt(max([6*r-p,1]))*R.S(end,end);
 %norm2 = svds(@(x,trans) firstOrderResidual(C0,S0,D0,Awave,dt,BC,x,trans,C,D),[1,1]*size(C,1),p);
 %fprintf('----!!! error is %e \n',norm(diag(S1(1:p,1:p)-norm2(1:p))));
 if quiet; fprintf('-- Adaptive: Residual computation time: %f\n',time1); end
@@ -38,9 +41,9 @@ if quiet; fprintf('-- Adaptive: Residual estimate of %e with tol %e\n',normF,tol
 if normF > tol %Add rank
     if quiet; fprintf('-- Adaptive: Increasing rank\n'); end
     tolflag = true;
-    dS1 = diag(S1);
+    dS1 = diag(R.S);
     for i=2:p %Seeing if the vectors I created are sufficient
-        normF = norm(dS1(i:pp),2) + sqrt(max([6*r-i,1]))*S1(pp,pp);
+        normF = norm(dS1(i:pp),2) + sqrt(max([6*r-i,1]))*R.S(pp,pp);
         if normF < tol
             tolflag = false;
             break
@@ -52,22 +55,22 @@ if normF > tol %Add rank
         q = i-1;
     end
     rr = q;
-    S1 = S1(1:q,1:q);
-    C1 = C1(:,1:q);
-    D1 = D1(:,1:q);
+    R.S = R.S(1:q,1:q);
+    R.C = R.C(:,1:q);
+    R.D = R.D(:,1:q);
     while tolflag == true
-        if rr+p >= size(C,1)
+        if rr+p >= size(R.C,1)
             tolflag = false;
         end       
-        Q = randn(size(C,1),pp);
-        Q = Rnt(Q) - LReval(C1,S1,D1,Q,'notransp');
+        Q = randn(size(R.C,1),pp);
+        Q = Rnt(Q) - LReval(R,Q,'notransp');
         [Q,~] = qr(Q,0);
-        B = Rt(Q)' - LReval(C1,S1,D1,Q,'transp')';
-        [Utmp,St,Dt] = svd(B,'econ');
-        Ct = Q*Utmp;
+        B = Rt(Q)' - LReval(R,Q,'transp')';
+        [Utmp,R_tmp.S,R_tmp.D] = svd(B,'econ');
+        R_tmp.C = Q*Utmp;
         for i=1:p %Seeing if the vectors I created are sufficient
-            dSt = diag(St);
-            normF = norm(dSt(i:pp),2) + sqrt(max([6*r-rr-i,1]))*St(pp,pp);
+            dSt = diag(R_tmp.S);
+            normF = norm(dSt(i:pp),2) + sqrt(max([6*r-rr-i,1]))*R_tmp.S(pp,pp);
             if normF < tol
                 tolflag = false;
                 break
@@ -79,20 +82,20 @@ if normF > tol %Add rank
             q = i-1;
         end
         rr = rr + q;
-        S1 = diag([diag(S1);diag(St(1:q,1:q))]);
-        C1 = [C1 Ct(:,1:q)];
-        D1 = [D1 Dt(:,1:q)];
+        R.S = diag([diag(R.S);diag(R_tmp.S(1:q,1:q))]);
+        R.C = [R.C R_tmp.C(:,1:q)];
+        R.D = [R.D R_tmp.D(:,1:q)];
     end
     if quiet; fprintf('-- Adaptive: Residual threshold requires %d new basis vectors\n',rr); end
-    [C,R_C] = qr([C C1],0);
-    [D,R_D] = qr([D D1],0);
+    [U.C,R_C] = qr([U.C R.C],0);
+    [U.D,R_D] = qr([U.D R.D],0);
     S_fill = zeros(r,rr);
-    R = R_C*[S S_fill;S_fill' S1]*R_D';
-    [S_C,Sig,S_D] = svd(R);
+    U.S = R_C*[S S_fill;S_fill' R.S]*R_D';
+    [S_C,Sig,S_D] = svd(U.S);
     r = sum(diag(Sig) > 1e-14);
-    C = C*S_C(:,1:r);
-    S = Sig(1:r,1:r);
-    D = D*S_D(:,1:r);
+    U.C = U.C*S_C(:,1:r);
+    U.S = Sig(1:r,1:r);
+    U.D = U.D*S_D(:,1:r);
     if quiet; fprintf('-- Adaptive: Cutting down to %d vectors, tol = %e\n',r,S(end,end)); end
 elseif normF < delta*tol %Need to decrease rank
     if quiet; fprintf('-- Adaptive: Decreasing rank\n'); end
@@ -118,7 +121,7 @@ else
 end
 end
 
-function Y = firstOrderResidual(C,S,D,Acell,dt,BC,X,trans,C1,D1)
+function Y = firstOrderResidual(U0,Acell,dt,BC,X,trans,U)
 %Compute action of first order residual
 %R(U) = (I-P_H)U - dt*(L(U)-P_H L(P_H U) + (I-P_H)F) 
 %where F = 0.5*(F(t_0)+F(t_1))
@@ -127,17 +130,17 @@ if BC.use
     MM = size(BC.cell1);
 end
 
-S_ = (C1'*C)*S*(D'*D1);
+S_ = (U.C'*U0.C)*U0.S*(U0.D'*U.D);
 
 if strcmp(trans,'notransp')
     %Y = zeros(size(X));
     
-    Y = C*S*(D'*X);
-    Y = Y - C1*S_*(D1'*X);
+    Y = U0.C*U0.S*(U0.D'*X);
+    Y = Y - U.C*S_*(U.D'*X);
     
     % Y = Y - (LU)*X
     for l=1:NN
-        Y = Y - dt*(Acell{l,1}*C)*S*(D'*Acell{l,2}'*X);
+        Y = Y - dt*(Acell{l,1}*U0.C)*U0.S*(U0.D'*Acell{l,2}'*X);
     end
     if BC.use
         %Y = Y - F*X;
@@ -148,21 +151,21 @@ if strcmp(trans,'notransp')
     %Projection
     %Y = Y + CC'*(LU)*DD'*X
     for l=1:NN
-        Y = Y + dt*C1*(C1'*Acell{l,1}*C1)*S_*(D1'*Acell{l,2}'*D1)*(D1'*X);
+        Y = Y + dt*U.C*(U.C'*Acell{l,1}*U.C)*S_*(U.D'*Acell{l,2}'*U.D)*(U.D'*X);
     end
     if BC.use
-        %Y = Y + C1*(C1'*F*D1)*(D1'*X);
+        %Y = Y + U.C*(U.C'*F*U.D)*(U.D'*X);
         for l=1:MM
-            Y = Y + dt*C1*(C1'*BC.cell1{l,1})*BC.cell1{l,2}*(BC.cell1{l,3}'*D1)*(D1'*X);
+            Y = Y + dt*U.C*(U.C'*BC.cell1{l,1})*BC.cell1{l,2}*(BC.cell1{l,3}'*U.D)*(U.D'*X);
         end
     end
 else
     %Y = zeros(size(X));
-    Y = D*S'*(C'*X);
-    Y = Y - D1*S_'*(C1'*X);
+    Y = U0.D*U0.S'*(U0.C'*X);
+    Y = Y - U.D*S_'*(U.C'*X);
     % Y = -(LU)'*X
     for l=1:NN
-        Y = Y - dt*(Acell{l,2}*D)*S'*(C'*Acell{l,1}'*X);
+        Y = Y - dt*(Acell{l,2}*U0.D)*U0.S'*(U0.C'*Acell{l,1}'*X);
     end
     if BC.use
         %Y = Y - F*X;
@@ -173,12 +176,12 @@ else
     %Projection
     %Y = Y + DD'*(LU)*CC'*X
     for l=1:NN
-        Y = Y + dt*D1*(D1'*Acell{l,2}*D1)*S_'*(C1'*Acell{l,1}'*C1)*(C1'*X);
+        Y = Y + dt*U.D*(U.D'*Acell{l,2}*U.D)*S_'*(U.C'*Acell{l,1}'*U.C)*(U.C'*X);
     end
     if BC.use
-        %Y = Y + D1*(D1'*F'*C1)*(C1'*X);
+        %Y = Y + U.D*(U.D'*F'*U.C)*(U.C'*X);
         for l=1:MM
-            Y = Y + dt*D1*(D1'*BC.cell1{l,3})*BC.cell1{l,2}'*(BC.cell1{l,1}'*C1)*(C1'*X);
+            Y = Y + dt*U.D*(U.D'*BC.cell1{l,3})*BC.cell1{l,2}'*(BC.cell1{l,1}'*U.C)*(U.C'*X);
         end
     end
     
@@ -187,75 +190,13 @@ end
 
 end
 
-function Y = AtranspA(C,S,D,Acell,dt,BC,X,C1,D1)
-    Y = firstOrderResidual(C,S,D,Acell,dt,BC,X,'notransp',C1,D1);
-    Y = -firstOrderResidual(C,S,D,Acell,dt,BC,Y,  'transp',C1,D1);
-end
-
-function Y = LReval(C,S,D,X,trans)
+function Y = LReval(U,X,trans)
 
 if strcmp(trans,'notransp')
-    Y = C*S*(D'*X);
+    Y = U.C*U.S*(U.D'*X);
 else
-    Y = D*S'*(C'*X);
+    Y = U.D*U.S'*(U.C'*X);
 end
-
-end
-
-function FU = computeFullGrid(C0,S0,D0,Acell,dt,BC)
-NN = size(Acell,1);
-if BC.use
-    MM = size(BC.cell1);
-end
-
-FU = C0*S0*D0';
-for l=1:NN
-    FU = FU - dt*Acell{l,1}*C0*S0*D0'*Acell{l,2}';
-end
-
-if BC.use
-    for l=1:MM
-        FU = FU - dt*BC.cell1{l,1}*BC.cell1{l,2}*BC.cell1{l,3}';
-    end
-end
-    
-end
-
-function [c,d] = ALTSVD(C0,S0,D0,C,S,D,Acell,dt,BC)
-
-    %Seed c and d
-    c = randn(size(C,1),1); c = c/norm(c,2);
-    d = randn(size(D,1),1); d = d/norm(d,2);
-    
-    for i=1:1000
-        %Update c
-        
-        R = (C0*S0)*(D0'*[D d]);
-        for l=1:size(Acell,1)
-            R = R - dt*(Acell{l,1}*C0)*S0*(D0'*Acell{l,2}'*[D d]);
-        end
-        R = R - C*S*(D'*[D d]);
-        %R = R - C*(C'*R);
-        [U,~,~] = svd(R,0);
-        c1 = U(:,1);
-        %c1 = c1 - C*(C'*c1);
-        
-        
-        %Update d
-        R = (D0*S0')*(C0'*[C c]);
-        for l=1:size(Acell,1)
-            R = R - dt*(Acell{l,2}*D0)*S0'*(C0'*Acell{l,1}'*[C c]);
-        end
-        R = R - D*S'*(C'*[C c]);
-        %R = R - D*(D'*R);
-        [U,~,~] = svd(R,0);
-        d1 = U(:,1);
-        %d1 = d1 - D*(D'*d1);
-        
-            
-        c = c1; c = c/norm(c,2);
-        d = d1; d = d/norm(d,2);
-    end
 
 end
 
